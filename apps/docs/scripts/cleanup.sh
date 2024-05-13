1874588dd9680bb8fca77ff863892ec003faa494
Tommy Nordli <tommy@nordli.io>
2024-05-13 15:22:07 +0200

    Add react-native-carplay package to root

--- apps/docs/scripts/cleanup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-scriptdir="$(dirname "$0")"
-
-# Remove unused md files
-rm "$scriptdir/../pages/docs/Home.md" || true
-rm "$scriptdir/../pages/docs/Exports.md" || true
-rm "$scriptdir/../pages/docs/_Sidebar.md" || true
-rm "$scriptdir/../pages/docs/.nojekyll" || true
